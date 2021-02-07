727b72b20c39f2dc4d7d77e0f19aa1e9a95a60d3
rnachaloor <rnachaloor@gmail.com>
2021-02-06 22:45:13 -0500

    Screens Stuff

---
+++ screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+//JSX (React HTML) goes here
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    );
+}
+
+//allows other files to see the login screen
+export default LoginScreen;
+
+//css goes in here
+const styles = StyleSheet.create({
+
+})
