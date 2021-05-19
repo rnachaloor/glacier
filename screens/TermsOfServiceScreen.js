855f13a8faff5b4b1665d6e3eb195964966529ef
aryan9204 <54330438+aryan9204@users.noreply.github.com>
2021-05-18 23:27:28 -0400

    Update TermsOfServiceScreen.js
    comments

--- screens/TermsOfServiceScreen.js
+++ screens/TermsOfServiceScreen.js
@@ -1,8 +1,13 @@
+//Imports
 import React, { Component, useState } from "react";
+
+//Imports component which essentially cntains all the code for the screen
 import TermsOfService from "../components/TermsOfService"
 
 const TermsOfServiceScreen = ({navigation}) => {
 
+  //Returns component
   return (<TermsOfService/>);}
 
+//Exports screen
 export default TermsOfServiceScreen;
