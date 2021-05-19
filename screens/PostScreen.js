16237ae07da6a684be7c8e3aab46db1848046e52
aryan9204 <54330438+aryan9204@users.noreply.github.com>
2021-05-18 23:43:38 -0400

    Update PostScreen.js
    comments

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,3 +1,4 @@
+//Imports
 import React, { Component, useState } from "react";
 import {
   StyleSheet,
@@ -11,10 +12,14 @@ import {
 import firestore, { firebase } from '@react-native-firebase/firestore';
 import {loggedIn, userId, user, name, firstName, lastName} from "./LoginScreen";
 import { Tile } from "react-native-elements";
+
+//Imports component which essentially contains all the code for the screen
 import PostForm from "../components/PostForm"
 
 const PostScreen = ({navigation}) => {
 
+  //Returns component
   return (<PostForm/>);}
 
+//Exports screen
 export default PostScreen;
