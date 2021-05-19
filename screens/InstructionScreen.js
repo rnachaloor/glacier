d46fd9f0d72447ccf87ff2cb3928db04752e1152
Aakash Reddy <54330393+AakashReddy22@users.noreply.github.com>
2021-05-18 23:20:42 -0400

    Commented InstructionScreen

--- screens/InstructionScreen.js
+++ screens/InstructionScreen.js
@@ -1,12 +1,15 @@
+// Importing react and components
 import React, { Component, useState } from "react";
 import { View } from "react-native";
+// Acquiring the InstructionsList component
 import InstructionsList from "../components/InstructionsLists"
 
+// Creating the Instruction Screen
 const InstructionScreen = ({navigation}) => {
-
   return (
     <View>
         <InstructionsList/>
     </View>);}
 
+// Exporting the InstructionScreen out
 export default InstructionScreen;
