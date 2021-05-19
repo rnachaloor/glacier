491a7438c1d558c6d9b576d031315ed4c1fa5aa7
aryan9204 <54330438+aryan9204@users.noreply.github.com>
2021-05-18 23:05:40 -0400

    Update ReportScreen.js

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -1,3 +1,4 @@
+//Imports
 import React, { Component, useState } from "react";
 import {
   StyleSheet,
@@ -11,10 +12,14 @@ import {
 import firestore, { firebase } from '@react-native-firebase/firestore';
 import {loggedIn, userId, user, name, firstName, lastName} from "./LoginScreen";
 import { Tile } from "react-native-elements";
+
+//Getting the component which essentially contains the code for the screen
 import ReportForm from "../components/ReportForm"
 
 const ReportScreen = ({navigation}) => {
 
+  //Returning the component
   return (<ReportForm/>);}
 
+//Exporting the screen
 export default ReportScreen;
